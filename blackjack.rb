dc9e28bd22669c70c1b06e34a29d9777e6ae8bd0
tesstryan <tesstryan@gmail.com>
2013-10-15 18:27:53 -0500

    2nd Attempt - Stuck on last challenge

--- blackjack.rb
+++ blackjack.rb
@@ -15,6 +15,7 @@
 # Example Output #3:
 # 2 A
 
+
 deck_of_cards = ["2","2","2","2",
          "3","3","3","3",
          "4","4","4","4",
@@ -39,58 +40,24 @@ value_of_cards = {'Ace' => 11,
                 '8' => 8,
                 '9' => 9,
                 '10' => 10,
-                'J' => 10,
-                'Q' => 10,
-                'K' => 10
+                'Jack' => 10,
+                'Queen' => 10,
+                'King' => 10
                 }
 
 card_1 = deck_of_cards.sample
 card_2 = deck_of_cards.sample
+hand_total = value_of_cards[card_1].to_i + value_of_cards[card_2].to_i
+
+# puts value_of_cards[card_1].to_i
+# puts value_of_cards[card_2].to_i
 
 puts "You have a #{card_1} and a #{card_2}."
 
-hand_value = value_of_cards.values_at(card_1, card_2).inject(:+)
-
-puts "You have a value of #{hand_value}."
-
-if hand_value > 21
-  puts "Dealer wins, better luck next time."
-end
-
-if hand_value == 21
-  puts "BLACKJACK! You have won."
-end
-
-if hand_value < 21
-  puts "Would you like to hit or stay?"
-  answer = gets.chomp.downcase
-    if answer.include?("hit") == true
-      card_3 = deck_of_cards.sample
-      puts "You now have #{card_1}, #{card_2} and a #{card_3}."
-      hand_value = value_of_cards.values_at(card_1, card_2, card_3).inject(:+)
-      puts "You have a value of #{hand_value}."
-        if hand_value > 21
-          puts "Dealer wins, better luck next time."
-        end
-
-        if hand_value == 21
-          puts "BLACKJACK! Congrats you have beat the dealer."
-        end
-
-        if hand_value < 21
-          puts "Would you like to hit or stay?"
-          answer = gets.chomp.downcase
-            if answer.include?("hit") == true
-              card_3 = deck_of_cards.sample
-              puts "You now have #{card_1}, #{card_2} and a #{card_3}."
-              hand_value = value_of_cards.values_at(card_1, card_2, card_3).inject(:+)
-              puts "You have a value of #{hand_value}."
-            end
-        end
-    end
-end
-
-
+# 2. Enhance #1 by showing the total score.
+# HINT: Consider all aces to have a value of 11.
+# Example Output #1:
+#
 # 5 8
 # You have 13.
 #
@@ -102,16 +69,7 @@ end
 # 2 A
 # You have 13.
 
-
-
-
-
-
-
-
-
-
-
+puts "You have #{hand_total}."
 
 # 3. Enhance #2 by adding a card if the score is less than 17.
 # Example Output #1:
@@ -122,9 +80,32 @@ end
 # Q A
 # You have 20.
 
+card_3 = deck_of_cards.sample
+card_4 = deck_of_cards.sample
+hand_total_hit = value_of_cards[card_1].to_i + value_of_cards[card_2].to_i + value_of_cards[card_3].to_i
+hand_total_hit_2 = value_of_cards[card_1].to_i + value_of_cards[card_2].to_i + value_of_cards[card_3].to_i + value_of_cards[card_4].to_i
 
+if hand_total < 17
+  puts "You have less than 17, you need to hit."
+  puts "Your next card is a #{card_3}. You now have #{hand_total_hit}."
+end
 
+if hand_total_hit < 17
+  puts "You still have less than 17, you need to hit again."
+  puts "Your next card is a #{card_4}. You now have #{hand_total_hit_2}."
+end
 
+if hand_total == 21
+  puts "Congratulations! You have blackjack!!"
+end
+
+if hand_total_hit == 21
+  puts "Congratulations! You have blackjack!!"
+end
+
+if hand_total_hit_2 == 21
+  puts "Congratulations! You have blackjack!!"
+end
 
 
 # 4. Enhance #3 by displaying a message if the score is over 21.
@@ -133,10 +114,17 @@ end
 # You have 24.
 # You're busted!
 
+if hand_total_hit_2 > 21
+   puts "BUSTED!"
+ end
 
+ if hand_total_hit > 21
+   puts "BUSTED!"
+ end
 
-
-
+ if hand_total > 21
+   puts "BUSTED!"
+ end
 
 
 # 5. Enhance #4 by considering an Ace to be 1 if it would prevent
@@ -144,3 +132,9 @@ end
 # Example Output #1:
 # 4 A 9
 # You have 14.
+
+
+
+
+
+
